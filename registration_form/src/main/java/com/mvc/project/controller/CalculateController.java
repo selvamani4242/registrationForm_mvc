1f5bfed606cccaaea7aa226a940c26805e962c96
Dinesh <selvamani4242@gmail.com>
2022-09-27 00:19:16 +0530

    calculate mvc form

--- registration_form/src/main/java/com/mvc/project/controller/CalculateController.java
+++ registration_form/src/main/java/com/mvc/project/controller/CalculateController.java
@@ -2,30 +2,28 @@ package com.mvc.project.controller;
 
 import com.mvc.project.dto.Calculateform;
 import com.mvc.project.dto.Calculateoutput;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class CalculateController {
+    @GetMapping("/calculate")
+    public String calculateForm(Model model) {
+        Calculateform calculate = new Calculateform();
+        model.addAttribute("calc", calculate);
+        return "Calculate";
+    }
 
-
-    @Controller
-    public class PersonController {
-        @GetMapping("/calculate")
-        public String greetingForm(Model model) {
-            Calculateform calculate = new Calculateform();
-            model.addAttribute("calc",  calculate);
-            return "Calculate";
-        }
-
-        @PostMapping("/calculate")
-        public String greetingSubmit(@ModelAttribute Calculateform calc, Model model) {
-            Calculateoutput calculateoutput= new Calculateoutput();
-            int outputview=calculateoutput.getNumber1()+calculateoutput.getNumber2();
-            model.addAttribute("outputview", outputview );
-            return "outputview";
-        }
+    @PostMapping("/calculate")
+    public String calculatesubmit(@ModelAttribute Calculateform calc, Model model) {
+        int output = calc.getNumber1() + calc.getNumber2();
+        model.addAttribute("sumoutput", output);
+        return "calculatorview";
     }
 }
+
